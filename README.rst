ba427f14eaf8ef46c4422f87adad1f65c1663991
Nirmit <nirmit.gupta@sanctumnetworks.com>
2018-08-08 15:22:04 +0530

    multiple file conversion update

--- README.rst
+++ README.rst
@@ -3,13 +3,17 @@
 Generating .so file using shell script
 ======================================
 
-- Copy the script file in the directory, which has .py file
+- pip install cython
+
+- Copy the script in the directory, which has .py file or folder
   to be converted to .so file.
 
 - run the script file
 	$./genso.sh
 
-- then give the file name e.g-sanctum.py to get the .so file.
+- then input 1 for file conversion or 2 for folder(having multiple .py files).
+
+- then enter the file name or folder name as asked in the prompt. 
 
 - the .so, .c and setup.py file will also be generated. 
 	 
--- genso.sh
+++ genso.sh
@@ -1,24 +1,61 @@
 #!/bin/bash
 
-echo "give file name"
-read fname                    #readin file name given with extension
+echo "Input:"
+echo "[1] for filename"
+echo "[2] for foldername"
+read input                    #readin file name or folder name given with extension
 
-#mv -- "$fname" "${fname%.py}.pyx"
+if (($input == 1))
+then
+    echo "Enter filename"
+    read fname
+    #appending commands to setup.py 
+    echo "from distutils.core import setup" >> setup.py               
+    echo "from Cython.Build import cythonize" >> setup.py
+    echo "setup(ext_modules = cythonize('$fname'))" >> setup.py
 
-#appending commands to setup.py 
-echo "from distutils.core import setup" >> setup.py               
-echo "from Cython.Build import cythonize" >> setup.py
-echo "setup(
-  ext_modules = cythonize('$fname')
-)" >> setup.py
+    #command for creating .so and .c file
+    python setup.py build_ext --inplace
 
-#command for creating .so and .c file
-python setup.py build_ext --inplace
+    sleep 5
+    echo "Conversion Sucessful"
+elif(($input == 2))
+then
+    #Creating .so of all .py file in the folder 
+    echo "Enter foldername" 
+    read foldername
+    cd $foldername
+    filename=$(ls  | grep .*.py$)
+    num_py=$(ls  | grep .*.py$| wc -l)
+    if (($num_py == 0))
+    then
+        echo "There are no .py files"
+    else
+        for file in $filename
+        do
+            echo "********************************************"
+            #appending commands to setup.py 
+            echo "from distutils.core import setup" >> setup.py               
+            echo "from Cython.Build import cythonize" >> setup.py
+            echo "setup(ext_modules = cythonize('$file'))" >> setup.py
 
-sleep 5
-echo " opration sucessful"
-
-
-
-
-
+            #command for creating .so and .c file
+            python setup.py build_ext --inplace
+        
+           sleep 3
+           echo ""
+           echo "$file converted to .so"
+           echo "********************************************"
+           echo "" 
+        done 
+        echo "Conversion Sucessful"
+    fi
+else
+    echo ""
+    echo "[ERROR] Wrong input"
+    echo "Input:"
+    echo "[1] for filename"
+    echo "[2] for foldername"
+    echo ""
+    echo "Please try again"
+fi
